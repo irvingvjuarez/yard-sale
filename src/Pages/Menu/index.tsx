c87dfe3bf8529a3961a96298a2646469e49364b9
Irving Juárez <irvingjuarez274@gmail.com>
2022-02-28 10:04:35 -0600

    The notifications in the menu already work

--- src/Pages/Menu/index.tsx
+++ src/Pages/Menu/index.tsx
@@ -3,6 +3,7 @@ import ArrowLeft from "../../Assets/Icons/arrow-left.svg";
 import { NavigateFunction, useNavigate } from "react-router-dom";
 
 export const Menu: React.FC<PageProps> = ({ state, dispatch }): JSX.Element => {
+  const { shoppingCart } = state
   const navigate: NavigateFunction = useNavigate()
   const handleClick = () => {
     navigate("/shopping-cart")
@@ -14,9 +15,17 @@ export const Menu: React.FC<PageProps> = ({ state, dispatch }): JSX.Element => {
       <article className="Menu__bar" onClick={handleClick}>
         <h2 className="Menu__title">Shopping Cart</h2>
 
-        <span>
-          <img src={ArrowLeft} alt="" />
-        </span>
+        <div className="Menu__buttons">
+          {(shoppingCart.length > 0) && (
+            <span className="Menu__notification">
+              <p>{shoppingCart.length < 10 ? shoppingCart.length : "+9"}</p>
+            </span>
+          )}
+
+          <span className="Menu__arrow-right">
+            <img src={ArrowLeft} alt="" />
+          </span>
+        </div>
       </article>
     </section>
   )
